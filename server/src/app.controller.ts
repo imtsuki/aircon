c1b245f0c334a00ab13420d01babb80e5471ebe6
imtsuki <me@qjx.app>
2020-05-10 22:44:30 +0800

    /auth/login
    Signed-off-by: imtsuki <me@qjx.app>

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Request,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -13,5 +26,16 @@ export class AppController {
   @Get('echo')
   getEcho(@Query('message') message: string): string {
     return message;
+  }
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: any) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) {
+    return req.user;
   }
 }
