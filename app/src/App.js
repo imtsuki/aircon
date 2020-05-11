3dbd89935de13f79f0049d0321543b6542f89fe0
imtsuki <me@qjx.app>
2020-05-11 13:32:17 +0800

    add routes
    Signed-off-by: imtsuki <me@qjx.app>

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { Row, Col, Layout, Result } from 'antd';
 import './App.css';
-import Login from './Login';
-
-const { Content } = Layout;
+import Routes from './Routes';
 
 const App = () => (
-  <Layout>
-    <Content>
-      <Result status="success" title="分布式温控系统" />
-      <Row>
-        <Col span={6} offset={9}>
-          <Login />
-        </Col>
-      </Row>
-    </Content>
-  </Layout>
+  <>
+    <Routes />
+  </>
 );
 
 export default App;
--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Input, Button, notification } from 'antd';
 
-import request from './utils/request';
+import request from '../utils/request';
 
 const layout = {
   labelCol: { span: 8 },
