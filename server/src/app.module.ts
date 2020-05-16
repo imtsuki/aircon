5f87dbf80228881faea8914c3430d2e6486ba11f
imtsuki <me@qjx.app>
2020-05-16 19:00:26 +0800

    ws and dynamic use cases
    Signed-off-by: imtsuki <me@qjx.app>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ClientController } from './client/client.controller';
 import { ApiController } from './api/api.controller';
+import { EventsGateway } from './events.gateway';
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController, ClientController, ApiController],
-  providers: [AppService],
+  providers: [AppService, EventsGateway],
 })
 export class AppModule {}
